ecd6377da4d4877479e33a74a9d7bdc332b8292b
Chris Dion <chris@verticallabs.ca>
2012-10-12 11:19:15 -0700

    cleanup

--- lib/adapters/gammu/connection.rb
+++ lib/adapters/gammu/connection.rb
@@ -35,38 +35,5 @@ module Adapters::Gammu
     def self.clear_received
       Adapters::Gammu::InboxItem.destroy_all
     end
-
-    def self.write
-      File.open(AppConfig.deployment.processes.gammu.config_file, 'w') {|f| f.write(text) }
-    end
-
-    def self.text
-      database_config = YAML.load_file(File.join(Rails.root, 'config', 'database.yml'))[Rails.env]
-      connection_config = self.load_config(:current)
-
-      config_string = <<-eos
-  [gammu]
-    port = #{connection_config.device}
-    #{connection_config.extra}
-  [smsd]
-    Service = sql
-    Driver = native_mysql
-    User = #{database_config['username']}
-    Password = #{database_config['password']}
-    PC = localhost
-    Database = #{database_config['database']}
-    Logfile = #{AppConfig.deployment.log_directory}/gammu.log
-    DeliveryReport = sms
-    ResetFrequency = 300
-    MaxRetries = 3
-    #DebugLevel = 2
-      eos
-
-      logger.debug("Setting gammu config")
-      logger.debug(config_string)
-
-      config_string
-    end
-
   end
 end
